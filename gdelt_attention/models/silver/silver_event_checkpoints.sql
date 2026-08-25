c8ceb487e4f816eb2d1d153e96cef2f576425b56
dragos1706 <dragostt@gmail.com>
2026-08-25 16:52:30 +0200

    added days_survived field to silver layer table, and updated documentation on the Domains by COuntry table

--- gdelt_attention/models/silver/silver_event_checkpoints.sql
+++ gdelt_attention/models/silver/silver_event_checkpoints.sql
@@ -9,13 +9,14 @@ WITH checkpoint_flags as (
     e.quad_class,
     e.goldstein_scale,
     e.avg_tone,
+    max(coalesce(date_diff(m.mention_date, e.event_date, day), 0)) as days_survived,
     date_diff(current_date(), e.event_date, day) as event_age_days,
     max(if(date_diff(m.mention_date, e.event_date, day) = 1, 1, 0))  as hit_day1,
     max(if(date_diff(m.mention_date, e.event_date, day) = 7, 1, 0))  as hit_day7,
     max(if(date_diff(m.mention_date, e.event_date, day) = 30, 1, 0)) as hit_day30,
     max(if(date_diff(m.mention_date, e.event_date, day) >= 1, 1, 0))  as surv_day1,
     max(if(date_diff(m.mention_date, e.event_date, day) >= 7, 1, 0))  as surv_day7,
-    max(if(date_diff(m.mention_date, e.event_date, day) >= 30, 1, 0)) as surv_day30
+    max(if(date_diff(m.mention_date, e.event_date, day) >= 30, 1, 0)) as surv_day30,
   FROM {{ ref('bronze_events') }} e
   LEFT JOIN {{ ref('bronze_mentions') }} m 
   ON e.event_id = m.event_id
